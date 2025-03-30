fa204f7bfdb61f0e77cce60552f4a3d9131abf81
Thomas Gebert <thomas@gebert.app>
2025-03-30 18:32:31 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::process::Command;
+use serde_json::{Result, Value};
+use std::collections::HashSet;
+fn main() -> Result<()> {
+    let output = 
+        Command::new("swaymsg")
+        .arg("-t")
+        .arg("get_workspaces")
+        .output()
+        .expect("failed to execute process");
+    let v: Value = serde_json::from_str(String::from_utf8_lossy(&output.stdout).as_ref())?;
+
+    if let Value::Array(arr) = v {
+        let b: HashSet<i64>= 
+            arr
+            .iter()
+            .filter_map(|workspace| workspace.get("num").and_then(Value::as_i64))
+            .collect();
+        for i in 1..20 {
+            if !b.contains(&i) {
+                Command::new("swaymsg")
+                    .arg("workspace")
+                    .arg(i.to_string())
+                    .output()
+                    .expect("failed to execute process");
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
